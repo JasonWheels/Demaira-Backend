4f4d4b94006ca13a4aa6cd13edc0a63e79d5a2a8
Jason Green <jasonjgreen86@gmail.com>
2019-04-01 21:32:49 -0500

    serializers changed from __all__ to individual fields

--- studio/serializers.py
+++ studio/serializers.py
@@ -4,26 +4,26 @@ from .models import School, Student, Teacher, DanceClass, JoinTeacherStudent
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
-        fields = '__all__'
-        # fields = ['id', 'name', 'email', 'address', 'phone_number', 'regular_class_price', 'private_class_price']
+        # fields = '__all__'
+        fields = ['id', 'name', 'email', 'address', 'phone_number', 'regular_class_price', 'private_class_price', 'description']
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
-        # fields = ['id', 'school_id', 'first_name', 'last_name', 'email', 'address', 'phone_number', 'join_date']
+        fields = ['id', 'school_id', 'student_first_name', 'student_last_name', 'parent_name', 'email', 'address', 'phone_number', 'join_date']
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
-        fields = '__all__'
-#         fields = ['id', 'school_id', 'first_name', 'last_name', 'email', 'address', 'phone_number', 'hourly_wage']
+        # fields = '__all__'
+        fields = ['id', 'school_id', 'first_name', 'last_name', 'email', 'address', 'phone_number', 'hourly_wage']
 
 class DanceClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = DanceClass
-        fields = '__all__'
-        # fields = ['id', 'school_id', 'primary_teacher', 'dance_type', 'level', 'description', 'is_private']
+        # fields = '__all__'
+        fields = ['id', 'school_id', 'teacher_id', 'dance_type', 'level', 'description', 'is_private']
 
 class JoinTeacherStudentSerializer(serializers.ModelSerializer):
     class Meta:
